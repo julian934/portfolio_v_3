650e4738956753c56a15d681734322e73ff81411
Julian Thatguy <jborner111@gmail.com>
2024-05-14 18:57:49 -0500

    check 3

--- src/app/utils/getData/contentful.ts
+++ src/app/utils/getData/contentful.ts
@@ -2,7 +2,7 @@ import {createClient,Entry} from 'contentful'
 
  const client=createClient({
     space:process?.env?.NEXT_PUBLIC_CONTENTFUL_SPACE_ID||'',
-    accessToken:process?.env?.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN || ''
+    accessToken:process?.env?.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN || 'zvnPASattsDHDSI6zMsZEFb6IJ9hJshqw-sRaqdF3eg'
 })
 /*interface ImageFields{
     title:string;
