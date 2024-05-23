07fb92ff22f9596e84d1c6f2f41857d99d24eacd
Julian Thatguy <jborner111@gmail.com>
2024-05-23 13:30:52 -0500

    added tokens

--- src/app/utils/getData/contentful.ts
+++ src/app/utils/getData/contentful.ts
@@ -1,8 +1,8 @@
 import {createClient,Entry} from 'contentful'
 
  const client=createClient({
-    space:process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID||'',
-    accessToken:process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN || ''
+    space:process?.env?.NEXT_PUBLIC_CONTENTFUL_SPACE_ID||'u8b3ckg07xkb',
+    accessToken:process?.env?.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN || 'zvnPASattsDHDSI6zMsZEFb6IJ9hJshqw-sRaqdF3eg'
 })
 /*interface ImageFields{
     title:string;
