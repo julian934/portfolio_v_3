900bdd31fb66a94d186fc3e5159dcfce1c8dcb52
Julian Thatguy <jborner111@gmail.com>
2024-05-23 13:25:43 -0500

    updated projects

--- src/app/components/footer/page.tsx
+++ src/app/components/footer/page.tsx
@@ -11,10 +11,10 @@ type Props = {}
 const Footer = (props: Props) => {
   return (
     <footer className=' flex  flex-row  justify-center max-sm:w-3/4 md:h-12 md:w-3/4 justify-around  ' >
-      <Link href='/' className='hover:bg-amber-300 rounded-xl h-10' ><Image className='flex ' src={LinkedIn} alt='LinkedIn' width={40} height={10} quality={100} /></Link>
-      <Link href='/' className='hover:bg-amber-300 rounded-xl h-10' ><Image className=' flex ' src={Instagram} alt='Instagram' width={40} height={10} quality={100} /></Link>
-      <Link href='/' className='hover:bg-amber-300 rounded-xl h-10' ><Image className=' flex ' src={Github} alt='Github' width={40} height={10} quality={100} /></Link> 
-      <Link href='/' className='hover:bg-amber-300 rounded-xl h-10' ><Image className='flex ' src={Twitter} alt='Twitter' width={40} height={10} quality={100} /></Link>
+      <Link href='https://www.linkedin.com/in/julian-borner-709b91b7/' className='hover:bg-amber-300 rounded-xl h-10' ><Image className='flex ' src={LinkedIn} alt='LinkedIn' width={40} height={10} quality={100} /></Link>
+      <Link href='https://www.instagram.com/jbthedev/' className='hover:bg-amber-300 rounded-xl h-10' ><Image className=' flex ' src={Instagram} alt='Instagram' width={40} height={10} quality={100} /></Link>
+      <Link href='https://github.com/julian934/' className='hover:bg-amber-300 rounded-xl h-10' ><Image className=' flex ' src={Github} alt='Github' width={40} height={10} quality={100} /></Link> 
+      
     </footer>
   )
 }
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -16,21 +16,34 @@ const Portfolio = (props: Props) => {
   //Add filters based on options
   const [currEntry, setEntries] = useState<any[]>([]);
    const [websiteLinks, setWebsiteLinks] = useState<any>([]);
+   const [isLoading, setIsLoading] = useState(true);
+   const [error, setError] = useState(null);
+ 
 
    useEffect(()=>{
       const fetchData=async()=>{
         try{
           const fetchedEntry:any=await fetchEntries();
-        setWebsiteLinks(fetchedEntry.slice(0,3));
+        setWebsiteLinks(fetchedEntry.slice(0,2));
         setEntries(fetchedEntry.slice(3))
         }catch(error){
           console.log(error)
+        }finally{
+          setIsLoading(false)
         }
         
       };
       
       fetchData();
    },[]);
+   if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  /*if (error) {
+    return <div>Error: {error?.message}</div>;
+  }
+  */
    process?.env?.NEXT_PUBLIC_CONTENTFUL_SPACE_ID && process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID;
    process?.env?.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN && process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN;
    console.log(currEntry)
@@ -47,7 +60,7 @@ const Portfolio = (props: Props) => {
 
       </div>))*/}
       {/* hard-set size is temporary */}
-      <div className='flex md:col-start-2 flex-col md:self-center md:justify-self-center md:row-start-2 md:row-span-2 md:col-span-2  md:h-[900px] md:w-[1200px] md:justify-around border-2 bg-white md:space-y-8 md:rounded-xl max-sm:space-y-8 max-sm:px-4' >
+      <div className='flex md:col-start-2 flex-col md:self-center md:justify-self-center md:row-start-2 md:row-span-2 md:col-span-2  md:h-[900px] md:w-[1200px] md:justify-around border-2 bg-white md:space-y-2 md:rounded-xl max-sm:space-y-8 max-sm:px-4' >
          <h1 className='flex bg-slate-400 md:w-24 opacity-75 text-white max-sm:w-28 max-sm:mt-8 ' >PORTFOLIO</h1>
         <div className='flex md:w-full flex-col' >
            
@@ -61,8 +74,10 @@ const Portfolio = (props: Props) => {
             {/*Customize visibility for small screens. */}
             {/* Once design is completed, add links.*/}
             <div className='flex md:h-3/4 max-sm:flex-col md:justify-around md:self-center md:w-full'>
-  {currEntry.map((entry, index) => (
+              {/* */}
+  {/*currEntry.map((entry, index) => (
     <div key={entry?.sys?.id} className='flex max-sm:w-full md:w-1/2 max-sm:h-full h-1/2 flex-col text-black hover:bg-slate-200 hover:bg-cover hover:opacity-75 rounded-xl'>
+    
       {websiteLinks[index]?.fields?.websiteLink?.content[0]?.content[0]?.value !== undefined && (
         <Link href={websiteLinks[index]?.fields?.websiteLink?.content[0]?.content[0]?.value} target='_blank'>
           {entry?.fields?.nuThreadImage?.fields?.file?.url && (
@@ -84,7 +99,28 @@ const Portfolio = (props: Props) => {
         </p>
       </div>
     </div>
-  ))}
+  ))*/}
+  {websiteLinks?.map((web:any)=>(
+  <div key={web?.fields?.sys?.id} className='flex max-sm:w-full md:w-1/2 max-sm:h-full h-1/2 flex-col text-black hover:bg-slate-200 hover:bg-cover hover:opacity-75 rounded-xl md:space-y-8 md:text-xl max-sm:text-xl' >
+    <h1 className='text-center' >{web?.fields?.nuThreadImage?.fields?.title}</h1>
+    {web?.fields?.websiteLink!==undefined && (
+      <Link className='md:flex md:justify-center' href={web?.fields?.websiteLink} >
+       
+        <Image className='border-2 md:w-96 max-sm:w-full max-sm:h-full md:h-80 self-center md:justify-center rounded-xl hover:bg-slate-200 hover:bg-cover'
+              width={100}
+              height={100}
+              quality={100} 
+              src={`https:${web?.fields?.nuThreadImage?.fields?.file?.url}`} alt={web?.fields?.nuThreadImage?.fields?.title} />
+      </Link>
+    )}
+    <div className='group relative self-center md:justify-center text-center md:self-center w-64 md:w-72 hover:bg-slate-200 max-sm:h-64 md:h-full left-20 md:left-0 md:-top-40' >
+      <p className='bg-amber-400 opacity-75 md:rounded-sm' >{web?.fields?.nuThreadImage?.fields?.title}</p>
+      <p className='text-center flex hidden group-hover:block group-hover:bg-blue group-hover:w-full group-hover:h-full hidden hover:block h-full w-full border-2 border-black text-black group-hover:bg-slate-100 md:rounded-md opacity-75' >
+        {web?.fields?.nuThreadImage?.fields?.description}
+      </p>
+    </div>
+
+  </div>))}
 </div>
            
             
